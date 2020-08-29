5f091bec48b53f512898f5dc9e940bb37973779f
Stefan <stefanroetscode@gmail.com>
2020-08-29 20:21:19 +0200

    hello world localhost:8080

--- todo-app/src/components/todo/WelcomeComponent.jsx
+++ todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,20 +1,47 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import HelloWorldService from '../../ api/todo/HelloWorldService.js'
 
 class WelcomeComponent extends Component {
+    constructor(props){
+        super(props)
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+        this.state = {
+            wellcomeMessage : ''
+        }
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Welcome!</h1>
                 <div className="container">
-                Welcome {this.props.match.params.name}. You can manage your todos <Link to="/todos">here</Link>.
+                    Welcome {this.props.match.params.name}.
+                    You can manage your todos <Link to="/todos">here</Link>.
+                </div>
+                <div className="container">
+                    Click here to get a customized welcome message
+                    <button onClick={this.retrieveWelcomeMessage}
+                        className="btn btn-success">Get Welcome Message</button>
+                </div>
+                <div className="container">
+                    {this.state.wellcomeMessage}
                 </div>
             </div>
         )        
     }
+
+    retrieveWelcomeMessage() {
+        HelloWorldService.executeHelloWorldService()
+        .then(response => this.handleSuccessfulResponse(response))
+        //.catch()
+    }
+
+    handleSuccessfulResponse(response) {
+        this.setState({wellcomeMessage: response.data})
+    }
 }
 
-function ErrorComponent() {
-    return <div>An Error Occurred. I don't know what to do! Contact support at abcd-efgh-ijkl</div>
-}
 export default WelcomeComponent
