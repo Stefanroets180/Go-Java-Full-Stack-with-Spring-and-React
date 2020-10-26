6fec76c9f319a85c64df5dccf1e22764af448cde
Stefan <stefanroetscode@gmail.com>
2020-10-26 11:05:52 +0200

    jpa in 10 steps

---
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/service/UserDAOService.java
@@ -0,0 +1,38 @@
+package com.in28minutes.learning.jpa.jpain10steps.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+
+@Repository
+@Transactional
+public class UserDAOService {
+	/* 
+	 User jack = new User("Jack", "Admin");
+	 
+	 User jill = new User("Jill", "Admin");
+	 
+	 entityManager.persist(jack);
+	 
+	 //Persistence Context
+	 jack.setRole("User");
+	 
+	 jill.setRole("User");
+	 
+	 //Only things that are persisted will be in the entityManager	 
+	 
+	 */
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public long insert(User user){
+		entityManager.persist(user);
+		return user.getId();
+	}
+
+}
